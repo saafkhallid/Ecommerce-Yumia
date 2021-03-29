12c26d2582211933f88d0b4ba90f43d18d18e904
khalid saaf <khallid.saaf@gmail.com>
2021-03-29 12:17:08 +0100

    debut de journer

---
+++ frontend/src/components/Navbar.js
@@ -0,0 +1,24 @@
+import './Navbar.css'
+import { Link } from "react-router-dom";
+
+export const Navbar = () => {
+    return (
+        <nav className="navbar">
+            {/* logo */}
+            <div className="navbar__log">
+                <h2>Yumia</h2>
+            </div>
+
+            { /* Link */}
+            <ul  className="navbar__link">
+                <Link>
+                
+                </Link>
+
+            </ul>
+
+            { }
+
+        </nav>
+    )
+}
